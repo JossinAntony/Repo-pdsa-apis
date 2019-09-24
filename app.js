92e9450c2f1a7465ba0d16f30101ae0159ab3c67
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-09-25 02:56:04 +0530

    corrspolicy changes and mongodb connect

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ app.use(Express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    // res.setHeader('Access-Control-Allow-Origin', 'https://studentdb1-jossin.herokuapp.com' ); // request origin/ name of app.
+    // ++change here++
+   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+     res.setHeader('Access-Control-Allow-Origin', 'https://pdsa-app.herokuapp.com' ); // request origin/ name of app.
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -29,13 +31,12 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
+// ++change here++
+// Mongoose.connect('mongodb://localhost:27017/SurveyDB', { useNewUrlParser: true }, (err, res) => {
+//     if (err) throw err;
+// });
 
-Mongoose.connect('mongodb://localhost:27017/SurveyDB', { useNewUrlParser: true }, (err, res) => {
-    if (err) throw err;
-    //console.log('Database online');
-});
-
-//Mongoose.connect('mongodb+srv://jossin:jossin@cluster0-arjkd.mongodb.net/test?retryWrites=true&w=majority');
+Mongoose.connect('mongodb+srv://jossin:jossin@cluster0-arjkd.mongodb.net/test?retryWrites=true&w=majority');
 
 const SurveySchema = Mongoose.model('surveyees', {
     sname: String,
